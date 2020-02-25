544f843ca798da4d9ced0b4e1fd6684eb07a7f3a
Tim Mendoza <tmendoza@twilio.com>
2020-02-25 11:04:46 -0700

    Update function responses

--- src/function.js
+++ src/function.js
@@ -7,20 +7,20 @@ const MAX_ALLOWED_SESSION_DURATION = 14400;
 module.exports.handler = (context, event, callback) => {
   const { TWILIO_ACCOUNT_SID, TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, API_SECRET, API_EXPIRY } = context;
 
-  const { identity, room_name, passcode } = event;
+  const { user_identity, room_name, passcode } = event;
 
   let response = new Twilio.Response();
 
   if (Date.now() > API_EXPIRY) {
     response.setStatusCode(401);
-    response.setBody({ code: 401, message: 'expired' });
+    response.setBody({ type: 'expired' });
     callback(null, response);
     return;
   }
 
   if (API_SECRET !== passcode) {
     response.setStatusCode(401);
-    response.setBody({ code: 401, message: 'unauthorized' });
+    response.setBody({ type: 'unauthorized' });
     callback(null, response);
     return;
   }
@@ -28,7 +28,7 @@ module.exports.handler = (context, event, callback) => {
   const token = new AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY_SID, TWILIO_API_KEY_SECRET, {
     ttl: MAX_ALLOWED_SESSION_DURATION
   });
-  token.identity = identity;
+  token.identity = user_identity;
   const videoGrant = new VideoGrant({ room: room_name });
   token.addGrant(videoGrant);
   response.setBody({ token: token.toJwt() });
