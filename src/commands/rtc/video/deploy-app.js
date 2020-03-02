6007f051a378f2a821dab4fb946ab67c7c5be0cc
Aaron Alaniz <aalaniz@twilio.com>
2020-03-02 16:37:12 -0600

    Add command documentation

--- src/commands/rtc/video/deploy-app.js
+++ src/commands/rtc/video/deploy-app.js
@@ -3,6 +3,34 @@ const { displayAppInfo, getAppInfo, deploy } = require('../../../helpers/helpers
 const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
 
 class DeployAppCommand extends TwilioClientCommand {
+  static usage = 'twilio rtc:video:deploy-app --authentication <auth>'
+
+  static description = `
+Deploy a Programmable Video app 
+
+This command publishes two components as a Twilio Function: an application token server and an optional React application.
+
+Token Server
+The token server provides Programmable Video access tokens and authorizes requests with the specified authentication mechanism.
+
+React Application
+The commands includes support for publishing a Programmable Video React Application. For more details using this plugin with the Programmable Video React application, please visit the project's home page. https://github.com/twilio/twilio-video-app-react
+`
+  
+  static examples = [
+    `
+# Deploy an application token server with passcode authentication
+$ twilio rtc:video:deploy-app --authentication passcode
+deploying app... done
+Passcode: 1111111111`,
+    `
+# Deploy an application token server with the React app
+$ twilio rtc:video:deploy-app --authentication passcode --app-directory /path/to/app
+deploying app... done
+Web App URL: https://video-app-1111-dev.twil.io?passcode=1111111111
+Passcode: 1111111111`,
+  ]
+
   async run() {
     await super.run();
 
