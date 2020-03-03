f2f32c6f57529c0615ba20be7820d8fd7c77d393
Tim Mendoza <tmendoza@twilio.com>
2020-03-03 12:02:46 -0700

    Add e2e test

--- test/e2e/e2e.test.js
+++ test/e2e/e2e.test.js
@@ -1,13 +1,13 @@
-const { test } = require('@oclif/test');
-const superagent = require('superagent');
+const { APP_NAME } = require('../../src/constants');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-const { APP_NAME } = require('../../src/constants');
+const superagent = require('superagent');
+const { test } = require('@oclif/test');
 
 jest.setTimeout(30000);
 
 function getPasscode(ctx) {
-  const match = ctx.stdout.match(/\nPasscode: (\d{10})/);
+  const match = ctx.stdout.match(/Passcode: (\d{10})/);
   return match ? match[1] : null;
 }
 
@@ -18,37 +18,39 @@ function getURL(passcode) {
 const testCLIPlugin = test
   .env({
     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
   })
-  .stdout();
+  .command(['rtc:video:delete-app']);
 
-describe('the RTC Twilio-CLI Plugin', () => {
-  beforeAll(() => {
-    testCLIPlugin.command(['rtc:video:delete-app']);
-  });
-
-  afterEach(() => {
-    testCLIPlugin.command(['rtc:video:delete-app']);
-  });
-
+describe('the RTC Twilio-CLI Plugin deploy command', () => {
   testCLIPlugin
+    .only()
+    .stdout()
     .command(['rtc:video:deploy-app', '--authentication', 'passcode'])
     .add('passcode', ctx => getPasscode(ctx))
+    .do(async ctx => {
+      try {
+        await superagent.get(`${getURL(ctx.passcode)}/`);
+      } catch (e) {
+        expect(e.status).toEqual(404);
+      }
+    })
     .do(async ctx => {
       const { body } = await superagent
         .post(`${getURL(ctx.passcode)}/token`)
         .send({ passcode: ctx.passcode, room_name: 'test-room', user_identity: 'test user' });
       expect(jwt.decode(body.token).grants).toEqual({ identity: 'test user', video: { room: 'test-room' } });
     })
-    .it('should deploy a token server');
+    .it('should deploy a token server without assets');
 
   testCLIPlugin
+    .stdout()
     .command([
       'rtc:video:deploy-app',
       '--authentication',
       'passcode',
       '--app-directory',
-      path.join(__dirname, 'test-assets')
+      path.join(__dirname, 'test-assets'),
     ])
     .add('passcode', ctx => getPasscode(ctx))
     .do(async ctx => {
@@ -60,4 +62,12 @@ describe('the RTC Twilio-CLI Plugin', () => {
       expect(text).toEqual('<html>test</html>');
     })
     .it('should deploy a token server with assets');
+
+  testCLIPlugin
+    .command(['rtc:video:deploy-app', '--authentication', 'passcode'])
+    .stdout()
+    .command(['rtc:video:deploy-app', '--authentication', 'passcode'])
+    .it('should not re-deploy without --override flag', ctx => {
+      expect(ctx.stdout).toContain('A Video app is already deployed.');
+    });
 });
