c5de2dd0c3c9397e0c07df843496fd9b57f54f84
Tim Mendoza <tmendoza@twilio.com>
2020-11-18 19:30:45 -0700

    Add tests for verify_passcode and fix tests for token

---
+++ test/serverless/assets/verify_passcode.test.js
@@ -0,0 +1,59 @@
+const verifyPasscode = jest.requireActual('../../../src/serverless/assets/verify_passcode');
+
+describe('the verify_passcode asset', () => {
+  it('should return an "unauthorized" error when the passcode is incorrect', () => {
+    Date.now = () => 5;
+    const mockCallback = jest.fn();
+    verifyPasscode(
+      { API_PASSCODE: '123456', API_PASSCODE_EXPIRY: '10', DOMAIN_NAME: 'video-app-1234-5678-dev.twil.io' },
+      { passcode: '9876543210' },
+      mockCallback
+    );
+
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      body: {
+        error: {
+          message: 'passcode incorrect',
+          explanation: 'The passcode used to validate application users is incorrect.',
+        },
+      },
+      headers: { 'Content-Type': 'application/json' },
+      statusCode: 401,
+    });
+  });
+
+  it('should return an "expired" error when the current time is past the API_PASSCODE_EXPIRY time', () => {
+    Date.now = () => 15;
+    const mockCallback = jest.fn();
+
+    verifyPasscode(
+      { API_PASSCODE: '123456', API_PASSCODE_EXPIRY: '10', DOMAIN_NAME: 'video-app-1234-5678-dev.twil.io' },
+      { passcode: '12345612345678', user_identity: 'test identity' },
+      mockCallback
+    );
+
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      body: {
+        error: {
+          message: 'passcode expired',
+          explanation:
+            'The passcode used to validate application users has expired. Re-deploy the application to refresh the passcode.',
+        },
+      },
+      headers: { 'Content-Type': 'application/json' },
+      statusCode: 401,
+    });
+  });
+
+  it('should not call the callback function when the passcode is correct and not expired', () => {
+    Date.now = () => 5;
+    const mockCallback = jest.fn();
+    verifyPasscode(
+      { API_PASSCODE: '123456', API_PASSCODE_EXPIRY: '10', DOMAIN_NAME: 'video-app-1234-5678-dev.twil.io' },
+      { passcode: '12345612345678' },
+      mockCallback
+    );
+
+    expect(mockCallback).not.toHaveBeenCalled();
+  });
+});
--- test/setupTests.js
+++ test/setupTests.js
@@ -20,3 +20,16 @@ class Response {
 
 global.Twilio = require('twilio');
 global.Twilio.Response = Response;
+
+const verifyPasscodePath = `${__dirname}/../src/serverless/assets/verify_passcode.js`;
+
+global.Runtime = {
+  getAssets: () => ({
+    '/auth-handler.js': {
+      path: verifyPasscodePath,
+    },
+  }),
+};
+
+// Mocking this as a no-op since this function is tested in 'tests/serverless/assets/verify_passcode.ts'.
+jest.mock(`${__dirname}/../src/serverless/assets/verify_passcode.js`, () => () => {});
